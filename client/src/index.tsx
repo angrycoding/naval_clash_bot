f591445b93df8f0e46a653c0369e0396eb95b95b
angrycoding <matveev.ruslan@gmail.com>
2023-10-09 00:31:04 +0300

    cleanup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -10,19 +10,6 @@ TelegramApi.expand();
 TelegramApi.setHeaderColor('#517DA2');
 TelegramApi.enableClosingConfirmation();
 
-
-// B1
-// http://localhost:3000/#tgWebAppData=start_param%3Dg188ga6f06X4I3F86c681EPH946j54U1a%3D%26user_id%3D31177503711
-// B2
-
-// http://localhost:3000/#tgWebAppData=start_param%3DsSESSION_ID%3D%26user_id%3D3117750371
-// @ts-ignore
-// 
-// http://localhost:3000/#tgWebAppData=start_param%3D188ga6f06X4I3F86c681EPH946j54U1a%3D%26user_id%3D311775037
-
-
-
-
 ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 ).render(
